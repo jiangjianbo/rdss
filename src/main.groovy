472594f367e9c791b66f115831d070bf866bd3e7
jiangjianbo <jiangjianbo@gmail.com>
2012-11-16 00:18:58 +0800

    add directory structure

--- src/main.groovy
+++ src/main.groovy
@@ -6,38 +6,41 @@
  * Time: 下午11:47
  * from https://www.ibm.com/developerworks/java/library/j-pg12144/index.html
  * http://www.javaworld.com/javaworld/jw-10-2004/jw-1004-groovy.html?page=2
+ * http://refcardz.dzone.com/refcardz/groovy
  */
 
 class Main {
-    ant = new AntBuilder()
-    base_dir = "path/to/my/DemoProject/"
-    src_dir = base_dir + "src"
-    lib_dir = base_dir + "lib"
-    build_dir = base_dir + "classes"
-    dist_dir = base_dir + "dist"
-    file_name = "whoami"
-    classpath = ant.path {
+    def ant = new AntBuilder()
+    def base_dir = "./"
+    def src_dir = base_dir + "src"
+    def lib_dir = base_dir + "lib"
+    def build_dir = base_dir + "classes"
+    def dist_dir = base_dir + "dist"
+    def file_name = "whoami"
+    def classpath = ant.path {
         fileset(dir: "${lib_dir}"){
             include(name: "*.jar")
         }
         pathelement(path: "${build_dir}")
     }
-    clean() {
+
+    def clean() {
         ant.delete(dir: "${build_dir}")
         ant.delete(dir: "${dist_dir}")
     }
-    build() {
+    def build() {
         ant.mkdir(dir: "${build_dir}")
         ant.javac(destdir: "${build_dir}", srcdir: "${src_dir}", classpath: "${classpath}")
     }
-    jar() {
+    def jar() {
         clean()
         build()
         ant.mkdir(dir: "${dist_dir}")
         ant.jar(destfile: "${dist_dir}/${file_name}.jar", basedir: "${build_dir}")
     }
+
     static void main(args) {
-        b = new Build()
+        b = new Main()
         b.run(args)
     }
     void run(args) {
